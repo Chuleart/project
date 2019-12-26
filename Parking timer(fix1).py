6428135220923fe34724d8bf43cb04215849f362
Chuleart <51635315+Chuleart@users.noreply.github.com>
2019-12-26 18:37:28 +0700

    Create Parking timer(fix1).py

---
+++ Parking timer(fix1).py
@@ -0,0 +1,29 @@
+import time
+timeis = time.localtime()
+a = time.strftime('%H''%M', timeis)
+#import เวลา
+Y = input("do you want parking Y or N")
+# ต้องการที่จอดรถไหม
+if Y <= Y:
+    print("start",a)
+else :
+    print("ok")
+c2 = input("do you want out parking :")
+#ออกและระบุเวลา
+total = (int(c2) - int(a))
+print(total,'min')
+#จอดกี่นาที่
+costPerHour = 20
+if total:
+  
+  hours = round(int(total/ 60))
+
+  total2 = round(hours * costPerHour, 2)
+
+  print('Your parking for ' + str(hours) + ' hours or ' + str(total) + ' minutes')
+
+  print('Total cost is ' + str(total2) + ' baht')
+
+else:
+
+  print('Error')
